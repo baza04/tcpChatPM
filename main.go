51d8102c5d95ea5411a5e728a211bb7af3035246
baza04 <bazarbay.amir@gmail.com>
2021-08-24 16:50:14 +0600

    feat: added simple tcp listener

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+)
+
+func main() {
+	port := flag.String("port", "8080", "custom port to tcp chat")
+	flag.Parse()
+	s := newServer()
+	go s.run()
+
+	listener, err := net.Listen("tcp", ":"+*port)
+	if err != nil {
+		log.Fatalf("unable to start server: %s", err.Error())
+	}
+	defer listener.Close()
+	log.Println("listen on: ", *port)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("unable to accept connection: %s", err.Error())
+			continue
+		}
+
+		go s.newClient(conn)
+	}
+}
